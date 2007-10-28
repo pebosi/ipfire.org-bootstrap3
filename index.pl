795d46bdfe2553354e80aa75c8165b333eeb9f9e
ms <ms@ea5c0bd1-69bd-2848-81d8-4f18e57aeed8>
2007-10-28 21:29:36 +0000

    Build some nice error pages.
    git-svn-id: http://svn.ipfire.org/svn/ipfire/branches/www.ipfire.org/trunk@1025 ea5c0bd1-69bd-2848-81d8-4f18e57aeed8

--- index.pl
+++ index.pl
@@ -1,19 +1,28 @@
 #!/usr/bin/perl
 
 my $lang = "en";
+my @error;
 
 # Set language to german, else use english site
 if ($ENV{'HTTP_ACCEPT_LANGUAGE'} =~ /^de(.*)/) {
 	$lang = "de";
 }
 
+if($ENV{'REQUEST_METHOD'} eq 'GET') {
+  @error = split(/=/, "$ENV{'QUERY_STRING'}");
+}
+
 print "Status: 302 Moved\n";
 print "Pragma: no-cache\n";
 
-if ($ENV{'SERVER_NAME'} eq "source.ipfire.org") {
-	print "Location: /$lang/source.shtml\n";
+if ($error[1]) {
+	print "Location: /$lang/$error[1].shtml\n";
 } else {
-	print "Location: /$lang/index.shtml\n";
+	if ($ENV{'SERVER_NAME'} eq "source.ipfire.org") {
+		print "Location: /$lang/source.shtml\n";
+	} else {
+		print "Location: /$lang/index.shtml\n";
+	}
 }
 
 # End http header...
