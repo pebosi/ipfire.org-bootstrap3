3ff59c7c2275adffd22bbaf3b813cdedf3591e9f
ms <ms@ea5c0bd1-69bd-2848-81d8-4f18e57aeed8>
2007-10-28 20:19:50 +0000

    Made a nice site for source.ipfire.org.
    git-svn-id: http://svn.ipfire.org/svn/ipfire/branches/www.ipfire.org/trunk@1023 ea5c0bd1-69bd-2848-81d8-4f18e57aeed8

--- index.pl
+++ index.pl
@@ -1,4 +1,13 @@
 #!/usr/bin/perl
 
 print "Status: 302 Moved\n";
-print "Location: /de/\n\n";
+print "Pragma: no-cache\n";
+
+if ($ENV{'SERVER_NAME'} eq "source.ipfire.org") {
+	print "Location: /en/source.shtml\n";
+} else {
+	print "Location: /en/index.shtml\n";
+}
+
+# End http header...
+print "\n";
