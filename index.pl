42aff73b4b42e78b2150ba6a9230021e353e080c
ms <ms@ea5c0bd1-69bd-2848-81d8-4f18e57aeed8>
2007-11-03 09:48:27 +0000

    Rewrite of the error pages of the main site for proper handling of the error codes. Every 404 returned 200... Google doesn't like that.
    git-svn-id: http://svn.ipfire.org/svn/ipfire/branches/www.ipfire.org/trunk@1048 ea5c0bd1-69bd-2848-81d8-4f18e57aeed8

--- index.pl
+++ index.pl
@@ -8,21 +8,13 @@ if ($ENV{'HTTP_ACCEPT_LANGUAGE'} =~ /^de(.*)/) {
 	$lang = "de";
 }
 
-if($ENV{'REQUEST_METHOD'} eq 'GET') {
-  @error = split(/=/, "$ENV{'QUERY_STRING'}");
-}
-
 print "Status: 302 Moved\n";
 print "Pragma: no-cache\n";
 
-if ($error[1]) {
-	print "Location: /$lang/$error[1].shtml\n";
+if ($ENV{'SERVER_NAME'} eq "source.ipfire.org") {
+	print "Location: /$lang/source.shtml\n";
 } else {
-	if ($ENV{'SERVER_NAME'} eq "source.ipfire.org") {
-		print "Location: /$lang/source.shtml\n";
-	} else {
-		print "Location: /$lang/index.shtml\n";
-	}
+	print "Location: /$lang/index.shtml\n";
 }
 
 # End http header...
