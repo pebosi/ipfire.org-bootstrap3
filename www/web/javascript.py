b3136945893ec66fb017c945ffc24df427f1c5c3
Michael Tremer <michael.tremer@ipfire.org>
2009-07-29 18:41:30 +0200

    Fix support for jqueryui.

--- www/web/javascript.py
+++ www/web/javascript.py
@@ -6,11 +6,14 @@ except ImportError:
 	import StringIO
 
 class Javascript(object):
-	def __init__(self, jquery=0):
+	def __init__(self, jquery=0, jqueryui=0):
 		self.io = StringIO.StringIO()
 
 		if jquery:
 			self.io.write("""<script src="/include/jquery.min.js" type="text/javascript"></script>""")
+		
+		if jqueryui:
+			self.io.write("""<script src="/include/jquery-ui.min.js" type="text/javascript"></script>""")
 	
 	def __call__(self, lang="en"):
 		return self.io.getvalue()
