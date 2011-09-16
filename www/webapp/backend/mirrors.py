a5e587c75bc86a066590e37f66c767af85928321
Michael Tremer <michael.tremer@ipfire.org>
2011-09-16 16:28:33 +0200

    mirrors: Only show enabled mirrors in overview.

--- www/webapp/backend/mirrors.py
+++ www/webapp/backend/mirrors.py
@@ -25,7 +25,7 @@ class Mirrors(object):
 		return Memcached()
 
 	def list(self):
-		return [Mirror(m.id) for m in self.db.query("SELECT id FROM mirrors ORDER BY state")]
+		return [Mirror(m.id) for m in self.db.query("SELECT id FROM mirrors WHERE disabled = 'N' ORDER BY state")]
 
 	def check_all(self):
 		for mirror in self.list():
